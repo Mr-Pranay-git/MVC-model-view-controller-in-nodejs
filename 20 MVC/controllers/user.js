349234e828a828dd23a5ed7d09c937e779dc33c0
Mr pranay-git <pranaymanusmare2@gmail.com>
2026-04-21 19:27:50 +0530

    perform some updates

--- 20 MVC/controllers/user.js
+++ 20 MVC/controllers/user.js
@@ -1,4 +1,5 @@
 const User = require("../models/user")
+const fs = require("fs");
 async function handleGetAllUsers(req, res) {
     const allDbUsers = await User.find({});
     return res.json(allDbUsers);
@@ -6,27 +7,30 @@ async function handleGetAllUsers(req, res) {
 
 async function handleGetUserById(req, res) {
     const id = Number(req.params.id);
-    const user = await users.find((user) => user.id === id);
+    const user = await User.findById(req.params.id);
     if (!user) return res.status(404).json({ error: "user not found" })
     return res.json(user);
 }
 
 async function handleUpdateUserById(req, res) {
     // Edit user with id
-    let id = Number(req.params.id);
-    let userToUpdate = await users.find(el => el.id === id);
+    let userToUpdate = await User.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        { new: true }
+    );
     if (!userToUpdate) {
         res.json({
             status: "fail",
             message: 'No user is found with ID ' + id
         })
     }
-    let index = users.indexOf(userToUpdate);
+    let index = User.indexOf(userToUpdate);
 
     Object.assign(userToUpdate, req.body);
-    users[index] = userToUpdate
+    User[index] = userToUpdate
 
-    fs.writeFile('MOCK_DATA.json', JSON.stringify(users), (err, data) => {
+    fs.writeFile('MOCK_DATA.json', JSON.stringify(User), (err, data) => {
         res.json({
             status: "success",
             data: {
@@ -36,54 +40,52 @@ async function handleUpdateUserById(req, res) {
     })
 }
 
-async function handleDeleteUserById(req, res){
+async function handleDeleteUserById(req, res) {
     //Delete user with id
-        let id = Number(req.params.id);
-        let userToDelete = users.find(el => el.id === id);
+    let id = Number(req.params.id);
+    let userToDelete = await User.findByIdAndDelete(req.params.id);
 
-        if(!userToDelete){
-            return res.json({
-                status:'fail',
-                message:'No user is found with ID '+ id + ' is found'
-            })
-        }
-        let index = users.indexOf(userToDelete);
+    if (!userToDelete) {
+        return res.json({
+            status: 'fail',
+            message: 'No user is found with ID ' + id + ' is found'
+        })
+    }
 
-        users.splice(index,1);
 
-        fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data)=>{
-            return res.status(204).json({           //204 stands for no content
-                status:"success",
-                data:{
-                    user: null
-                }
-            })
-        })    
+    fs.writeFile("./MOCK_DATA.json", JSON.stringify(User), (err, data) => {
+        return res.status(204).json({           //204 stands for no content
+            status: "success",
+            data: {
+                user: null
+            }
+        })
+    })
 }
 
-async function handleCreateNewUser(req,res){
+async function handleCreateNewUser(req, res) {
     const body = req.body;
-        if(
-            !body.firstName || 
-            !body.lastName || 
-            !body.email || 
-            !body.gender ||
-            !body.job_title
-        ){
-            return res.status(400).json({msg: "All fields are req..."})
-        }
+    if (
+        !body.firstName ||
+        !body.lastName ||
+        !body.email ||
+        !body.gender ||
+        !body.job_title
+    ) {
+        return res.status(400).json({ msg: "All fields are req..." })
+    }
 
-        const result = await User.create({
-            firstName: body.firstName,
-            lastName: body.lastName,
-            email: body.email,
-            gender: body.gender,
-            jobTitle: body.job_title
-        })
+    const result = await User.create({
+        firstName: body.firstName,
+        lastName: body.lastName,
+        email: body.email,
+        gender: body.gender,
+        jobTitle: body.job_title
+    })
 
-        console.log("result: ", result);
+    console.log("result: ", result);
 
-        return res.status(201).json({msg: 'success', id: result._id});
+    return res.status(201).json({ msg: 'success', id: result._id });
 }
 
 module.exports = {
