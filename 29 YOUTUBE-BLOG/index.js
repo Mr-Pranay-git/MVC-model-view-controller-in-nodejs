eb715b894c86347d1dc5182b43a333e4af8c460f
Mr pranay-git <pranaymanusmare2@gmail.com>
2026-06-20 23:52:41 +0530

    making blogin site

---
+++ 29 YOUTUBE-BLOG/index.js
@@ -0,0 +1,17 @@
+const path = require("path");
+const express = require('express');
+
+const userRoute = require('./routes/user')
+
+const app = express();
+const PORT = 8000;
+
+app.set('view engine', 'ejs');
+app.set("views", path.resolve("./views"));
+
+app.get('/', (req,res)=>{
+    res.render('home');
+});
+app.use("/user", userRoute)
+
+app.listen(PORT, () => console.log(`server Started at PORT: ${PORT}`))
