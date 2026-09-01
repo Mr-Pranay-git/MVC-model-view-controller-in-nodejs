e0f07248d72969208c7254001999109942d264b0
Mr pranay-git <pranaymanusmare2@gmail.com>
2026-09-01 23:43:29 +0530

    webSocket in progress

--- web sockets/server.js
+++ web sockets/server.js
@@ -1,6 +1,27 @@
 import http from 'node:http';
+import fs from 'node:fs/promises';
+import path from 'path';
+
+import { WebSocketServer } from 'ws';
+
 const PORT = process.env.PORT ?? 9000;
 
-const httpServer = http.createServer(async function(req, res){
-    
-})
+const httpServer = http.createServer(async function (req,res){
+    const indexFile = await fs.readFile(path.resolve('./index.html'), 'utf-8')
+    res.setHeader('Content-Type', 'text/html');
+    return res.end(indexFile);
+});
+const wsServer = new WebSocketServer({server: httpServer});
+
+wsServer.on('connection', (websocket)=>{
+    console.log('Websocket Connection....');
+
+    websocket.on('message', (data)=>{
+        console.log(`WebSocket Message Recv.`, data.toString());
+        websocket.send('pong.. hello ji from server')
+    });
+});
+
+httpServer.listen(PORT, ()=>{
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
